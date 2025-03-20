36ba8e1ee2d48d18d5eafdcb29abacd616bd335d
R3ndrex <yacholohchtoli@gmail.com>
2025-03-20 15:52:59 +0200

    Scroll to start of page,dont handle click when max or min page already

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,24 +5,49 @@ import { ChevronDoubleRightIcon } from "@heroicons/react/24/solid";
 
 export default function Pagination({ setter, value, min, max }) {
     function handleLeftClick() {
-        if (value !== min) setter((prev) => --prev);
+        if (value !== min) {
+            setter((prev) => --prev);
+            window.scrollTo(0, 0);
+        }
     }
+    function handleLeftMaxClick() {
+        if (value !== min) {
+            setter(min);
+            window.scrollTo(0, 0);
+        }
+    }
+
     function handleRightClick() {
-        if (value !== max) setter((prev) => ++prev);
+        if (value !== max) {
+            setter((prev) => ++prev);
+            window.scrollTo(0, 0);
+        }
+    }
+    function handleRightMaxClick() {
+        if (value !== max) {
+            setter(max);
+            window.scrollTo(0, 0);
+        }
     }
 
     return (
-        <div className="flex gap-[1rem] justify-center m-[1rem] items-center">
+        <div className="flex gap-[1rem] justify-center m-[1rem] items-center ">
             <ChevronDoubleLeftIcon
-                onClick={() => setter(min)}
-                className="h-[2rem]"
+                onClick={handleLeftMaxClick}
+                className="h-[2rem] cursor-pointer"
             />
-            <ChevronLeftIcon onClick={handleLeftClick} className="h-[2rem]" />
+            <ChevronLeftIcon
+                onClick={handleLeftClick}
+                className="h-[2rem] cursor-pointer"
+            />
             <div className="font-bold">{value + 1}</div>
-            <ChevronRightIcon onClick={handleRightClick} className="h-[2rem]" />
+            <ChevronRightIcon
+                onClick={handleRightClick}
+                className="h-[2rem] cursor-pointer"
+            />
             <ChevronDoubleRightIcon
-                onClick={() => setter(max)}
-                className="h-[2rem]"
+                onClick={handleRightMaxClick}
+                className="h-[2rem] cursor-pointer"
             />
         </div>
     );
