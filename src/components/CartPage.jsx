b4934f357530a49bf77f3281f9c6ec030875aec9
R3ndrex <yacholohchtoli@gmail.com>
2025-03-20 15:53:44 +0200

    add cartpage with selecteditems full cost and add item button

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,7 +1,50 @@
+import { useMemo } from "react";
+import { useOutletContext } from "react-router-dom";
+
 export default function CartPage() {
+    const [setSelectedItems, selectedItems] = useOutletContext();
+    const allItemsPrice = useMemo(
+        () =>
+            selectedItems.reduce(
+                (prev, current) => prev + current.price * current.amount,
+                0
+            ),
+        [selectedItems]
+    );
+    function handleAdd(item) {
+        setSelectedItems((prev) => {
+            const existingItem = prev.find(
+                (element) => element.title === item.title
+            );
+
+            if (existingItem) {
+                return prev.map((element) =>
+                    element.title === item.title
+                        ? { ...element, amount: element.amount + 1 }
+                        : element
+                );
+            }
+            return;
+        });
+    }
     return (
         <>
-            <h1>This is your cart</h1>
+            <ul>
+                {selectedItems.map((item) => (
+                    <li className="item-li-shopping-cart">
+                        <h2>{item.title}</h2>
+                        <img src={item.image} alt={item.title} />
+                        <p>{item.description}</p>
+                        <div>
+                            Amount: {item.amount}{" "}
+                            <button onClick={() => handleAdd(item)}>Add</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+            <div>
+                Cost: <strong> {allItemsPrice} $</strong>
+            </div>
         </>
     );
 }
