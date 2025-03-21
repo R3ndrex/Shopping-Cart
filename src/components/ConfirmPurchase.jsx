a9796c21024eada308310ff6cfbcc1ceb29d0697
R3ndrex <yacholohchtoli@gmail.com>
2025-03-21 23:26:31 +0200

    make container to display only dialog and be only clickable item on screen

--- src/components/ConfirmPurchase.jsx
+++ src/components/ConfirmPurchase.jsx
@@ -7,11 +7,13 @@ export default function ConfirmPurchase({ show, setShow, setSelectedItems }) {
     if (show) {
         return (
             <dialog open className="purchase-dialog">
-                <h1>We need you to confirm this purchase</h1>
-                <p>Please review your order details before confirming.</p>
-                <div className="flex gap-[1rem]">
-                    <button onClick={handleBuying}>Confirm Purchase</button>
-                    <button onClick={() => setShow(false)}>Exit</button>
+                <div className="confirm-container">
+                    <h1>We need you to confirm this purchase</h1>
+                    <p>Please review your order details before confirming.</p>
+                    <div className="flex gap-[1rem]">
+                        <button onClick={handleBuying}>Confirm Purchase</button>
+                        <button onClick={() => setShow(false)}>Exit</button>
+                    </div>
                 </div>
             </dialog>
         );
