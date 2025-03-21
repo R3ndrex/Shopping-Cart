f48817903c1c8554f461e58f1dc086c357d7ea7d
R3ndrex <yacholohchtoli@gmail.com>
2025-03-21 18:14:28 +0200

    Centralize text for when cart is empty

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -80,7 +80,7 @@ export default function CartPage() {
                     />
                 </main>
             ) : (
-                <main className="flex flex-col gap-[1rem] items-center m-[2rem]">
+                <main className="flex flex-col gap-[1rem] items-center m-[2rem] text-center">
                     <h1>Your cart is empty </h1>
                     <button onClick={() => navigate("/store")}>
                         Go to store page
