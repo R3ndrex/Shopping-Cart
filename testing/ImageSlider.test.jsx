ae18a9b3ca45ca6e55eb1212daba73de7d1c0597
R3ndrex <yacholohchtoli@gmail.com>
2025-03-21 12:50:14 +0200

    Add tests to imageslider like click next and previous page

--- testing/ImageSlider.test.jsx
+++ testing/ImageSlider.test.jsx
@@ -2,25 +2,61 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import ImageSlider from "../src/components/ImageSlider";
 import userEvent from "@testing-library/user-event";
+
 describe("ImageSlider", () => {
+    it("render correct first image", () => {
+        render(
+            <ImageSlider images={["image1", "image2", "image3"]} alt="test" />
+        );
+
+        expect(screen.getByRole("img").getAttribute("src")).toEqual("image1");
+    });
+
+    it("goes to next image after clicking on right chevron", async () => {
+        const user = userEvent.setup();
+        render(<ImageSlider images={["image1", "image2"]} alt="test" />);
+        const rightChevron = screen.getByTestId("right-chevron");
+
+        await user.click(rightChevron);
+
+        expect(screen.getByRole("img").getAttribute("src")).toEqual("image2");
+    });
+
+    it("goes to previous image after clicking on left chevron", async () => {
+        const user = userEvent.setup();
+        render(
+            <ImageSlider images={["image1", "image2", "image3"]} alt="test" />
+        );
+        const leftChevron = screen.getByTestId("left-chevron");
+        const rightChevron = screen.getByTestId("right-chevron");
+
+        await user.click(rightChevron);
+        await user.click(rightChevron);
+        await user.click(leftChevron);
+        await user.click(leftChevron);
+
+        expect(screen.getByRole("img").getAttribute("src")).toEqual("image1");
+    });
+
     it("goes to last image after clicking on first image left chevron", async () => {
         const user = userEvent.setup();
         render(
             <ImageSlider images={["image1", "image2", "image3"]} alt="test" />
         );
 
-        expect(screen.getByRole("img").getAttribute("src")).toEqual("image1");
         await user.click(screen.getByTestId("left-chevron"));
+
         expect(screen.getByRole("img").getAttribute("src")).toEqual("image3");
     });
+
     it("goes to first image after clicking on last image right chevron", async () => {
         const user = userEvent.setup();
         render(<ImageSlider images={["image1", "image2"]} alt="test" />);
         const rightChevron = screen.getByTestId("right-chevron");
 
-        expect(screen.getByRole("img").getAttribute("src")).toEqual("image1");
         await user.click(rightChevron);
         await user.click(rightChevron);
+
         expect(screen.getByRole("img").getAttribute("src")).toEqual("image1");
     });
 });
