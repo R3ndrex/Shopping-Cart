bfdfa653d5a2d2aee575629173d8aa153d1df92d
R3ndrex <yacholohchtoli@gmail.com>
2025-03-24 23:56:10 +0200

    Fix not showing image of item

--- src/components/CartPageItem.jsx
+++ src/components/CartPageItem.jsx
@@ -35,7 +35,7 @@ export default function CartPageItem({ item }) {
     return (
         <li className="item-li-shopping-cart" key={item.id}>
             <h2>{item.title}</h2>
-            <img src={item.image} alt={item.title} />
+            <img src={item.images[0]} alt={item.title} />
             <p>{item.description}</p>
             <div className="flex gap-[1rem] text-center items-center pb-[1rem] pt-[1rem]">
                 <TrashIcon
