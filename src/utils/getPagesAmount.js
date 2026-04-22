136e050cba8e6c47f6982026b5c0a51143bfee55
R3ndrex <yacholohchtoli@gmail.com>
2026-04-22 20:43:03 +0300

    get all products

--- src/utils/getPagesAmount.js
+++ src/utils/getPagesAmount.js
@@ -1,7 +1,9 @@
 import { ITEMS_PER_PAGE } from "./consts";
 export default async function getPagesAmount() {
     try {
-        const res = await fetch("https://api.escuelajs.co/api/v1/products");
+        const res = await fetch(
+            "https://api.escuelajs.co/api/v1/products?offset=0&limit=9000",
+        );
         const data = await res.json();
         return Math.floor(data.length / ITEMS_PER_PAGE);
     } catch (e) {
