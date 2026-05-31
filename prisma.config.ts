ca1cb72975bdbe814a58044d8dd1ca492187d88a
R3ndrex <yacholohchtoli@gmail.com>
2026-05-31 11:58:25 +0300

    add check for db url

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,17 @@
 import "dotenv/config";
 import { defineConfig } from "@prisma/config";
 
+const DB_URL = process.env.DATABASE_URL;
+if (!DB_URL) {
+    throw new Error("DB URL is not defined");
+}
+
 export default defineConfig({
     schema: "prisma/schema.prisma",
     migrations: {
         path: "prisma/migrations",
     },
     datasource: {
-        url: process.env["DATABASE_URL"],
+        url: DB_URL,
     },
 });
